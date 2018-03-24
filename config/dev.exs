7f5a6a4a291ef5bf7d128ad60b33d2ab15aeb386
Amit Srivastava <link.enamit@gmail.com>
2018-03-24 20:01:32 +0530

    changed db name

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,14 @@ config :akcommerce, AkcommerceWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: [
+    node: [
+      "node_modules/brunch/bin/brunch",
+      "watch",
+      "--stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
 
 # ## SSL Support
 #
@@ -53,6 +59,6 @@ config :akcommerce, Akcommerce.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
-  database: "akcommerce_dev",
+  database: "akcommerce",
   hostname: "localhost",
   pool_size: 10
